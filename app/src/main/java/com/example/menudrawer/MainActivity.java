ec347a2bb727a8debd87ce379209a7c0c9af40ea
misael <misael111998@gmail.com>
2019-03-25 08:19:28 -0600

    Initial commit

---
+++ app/src/main/java/com/example/menudrawer/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.menudrawer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void siguiente(View view) {
+        Intent i=new Intent(MainActivity.this,menu.class);
+        startActivity(i);
+    }
+}
